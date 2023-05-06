625e498869ee3a9a838752a3863db5ec1c0900a5
conceptroot <conceptroot@gmail.com>
2023-05-06 23:09:25 +0530

    refactor: rewrite 04 task. Recursive function + modul default export

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -10,29 +10,33 @@ const currentDirname = path.dirname(currentFilename)
 const sourcePath = path.join(currentDirname, SOURCE_DIR)
 const targetPath = path.join(currentDirname, TARGET_DIR)
 
-try {
-    // Create (if not exist) target dir
-    await fs.mkdir(targetPath, {recursive: true})
-    // Read source path
-    const sourceFiles = await fs.readdir(sourcePath)
-    console.log('Source path contains:', sourceFiles)
-    // Copy files
-    await copyFiles(sourcePath, targetPath, sourceFiles)
 
-}
-catch (e){
-    console.log('ОШИБКА')
-    throw(e)
-}
+await processDir(sourcePath, targetPath)
 
-async function copyFiles(sourcePath, targetPath, files) {
-    for (let file of files) {
-        const source = path.join(sourcePath, file)
-        const target = path.join(targetPath, file)
-        fs.copyFile(source, target)
+
+export default async function processDir(sourceDir, targetDir) {
+    // Сначала создаем директорию targetDir куда будут записываться файлы
+    await fs.mkdir(targetDir, {recursive: true})
+    // Читаем всю директорию sourceDir, получаем список файлов
+    const sourceFilesAndDirs = await fs.readdir(sourceDir, {withFileTypes: true})
+    // Итерируемся по списку fileAndDirs:
+    for (let item of  sourceFilesAndDirs) {
+        if (item.isFile()) {
+            // Если файл, то копируем файл в нужную директорию targetDir
+            console.log('ya fail:', item.name)
+            const sourceFileName = path.join(sourceDir, item.name)
+            const targetFileName = path.join(targetDir, item.name)
+            await fs.copyFile(sourceFileName, targetFileName)
+        } 
+        if (item.isDirectory()) {
+            // Если директория, то:
+            // newDir = создаем эту директорию используя targetDir + имядиректории
+            const newTargetDir = path.join(targetDir, item.name)
+            const newSourceDir = path.join(sourceDir, item.name)
+            await fs.mkdir(newTargetDir, {recursive: true})
+            // Вызываем функцию processDir(fileAndDirs, targetDir+fileAndDirs)
+            await processDir(newSourceDir, newTargetDir)
+        }
     }
 }
-
-
-
 
